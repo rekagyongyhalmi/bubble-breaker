93389c6e0c7e6e221cdff73da011c2a1629e67c5
Réka Gyöngyhalmi <31566539+rekagyongyhalmi@users.noreply.github.com>
2023-05-29 20:30:46 +0200

    color block
    adjacent bubbles of the same color get removed when player clicks

--- bubblebreaker.js
+++ bubblebreaker.js
@@ -1,4 +1,5 @@
 let bubbleColorPalette = ["red", "green", "blue", "yellow"];
+let backgroundColor = "turquoise";
 let numberOfRows = 10;
 let numberOfColumns = 15;
 
@@ -38,8 +39,89 @@ function newGame() {
 
 }
 
+let score = 0;
+let colorBlock = new Set();
+let colorClicked = "";
+
+
 function bubbleClicked() {
-    let colorClicked = this.getAttribute("fill");
-    console.log(colorClicked);
-    this.remove();
+    colorClicked = this.getAttribute("fill");
+    getAdjacents(this);
+    if (colorBlock.size > 1) {
+        console.log("Block: " + colorBlock.size);
+        colorBlock.forEach(element => {
+            element.setAttribute("fill", backgroundColor);
+            element.removeEventListener('click', bubbleClicked);
+        });
+        score += colorBlock.size * (colorBlock.size - 1);
+        document.getElementById("score").innerHTML = score;
+        //bubbleShift();
+    }
+    colorBlock = new Set();
+    colorClicked = "";
+}
+
+function getAdjacents(startBubble) {
+    if (!(colorBlock.has(startBubble))) {
+        colorBlock.add(startBubble);
+        let bubbleId = startBubble.id;
+        console.log(bubbleId, colorClicked);
+
+        let startingX = parseInt(bubbleId.split("-")[0]);
+        //console.log(startingX);
+        let startingY = parseInt(bubbleId.split("-")[1]);
+        //console.log(startingY);
+        lookUp(startingX, startingY);
+        lookDown(startingX, startingY);
+        lookLeft(startingX, startingY);
+        lookRight(startingX, startingY);
+    }
+}
+
+// Check if bubble above is of the same color as bubble clicked
+function lookUp(x, y) {
+    if (y > 0) {
+        let bubbleAbove = document.getElementById(`${x}-${y - 1}`);
+        console.log("on top:" + bubbleAbove.id);
+        if (bubbleAbove.getAttribute("fill") == colorClicked) {
+            getAdjacents(bubbleAbove);
+        }
+    }
+}
+
+function lookDown(x, y) {
+    if (y < numberOfRows - 1) {
+        let bubbleBelow = document.getElementById(`${x}-${y + 1}`);
+        console.log("below:" + bubbleBelow.id);
+        if (bubbleBelow.getAttribute("fill") == colorClicked) {
+            getAdjacents(bubbleBelow);
+        }
+    }
+}
+
+function lookLeft(x, y) {
+    if (x > 0) {
+        let bubbleToLeft = document.getElementById(`${x - 1}-${y}`);
+        console.log("to left:" + bubbleToLeft.id);
+        if (bubbleToLeft.getAttribute("fill") == colorClicked) {
+            getAdjacents(bubbleToLeft);
+        }
+    }
+}
+
+function lookRight(x, y) {
+    if (x < numberOfColumns - 1) {
+        let bubbleToRight = document.getElementById(`${x + 1}-${y}`);
+        console.log("to right:" + bubbleToRight.id);
+        if (bubbleToRight.getAttribute("fill") == colorClicked) {
+            getAdjacents(bubbleToRight);
+        }
+    }
+}
+
+function bubbleShift() {
+    let remainingBubbles = document.querySelectorAll(circle);
+    remainingBubbles.forEach(element => {
+        // Now what???
+    });
 }
